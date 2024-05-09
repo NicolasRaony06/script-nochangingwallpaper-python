fc3a1d4bc15c90e9ffda44e0f6faa3ad4e238063
Nicolas Raony Dantas Queiroz <163556444+NicolasRaony06@users.noreply.github.com>
2024-05-09 13:54:51 -0300

    Adicionando sripts

---
+++ papel_parede.py
@@ -0,0 +1,61 @@
+import os
+import subprocess
+
+def get_user_sid(username):
+    comandos = []
+    for name in username:
+        comandos.append(f'wmic useraccount where name="{name}" get sid')
+    
+    saidas = []
+    for comando in comandos:
+        saidas.append(f"{(subprocess.check_output(comando, shell=True, text=True)).strip('SID').strip().strip()}")
+
+    return saidas
+
+def run_regedit():
+    os.system("start regedit.exe")
+
+def create_registry_key(path, key_name):
+    try:
+        import winreg as reg
+    except ImportError:
+        import _winreg as reg
+
+    key = reg.CreateKey(reg.HKEY_USERS, path)
+    reg.CreateKey(key, key_name)
+    reg.CloseKey(key)
+
+def create_registry_value(path, value_name, value_data):
+    try:
+        import winreg as reg
+    except ImportError:
+        import _winreg as reg
+
+    key = reg.OpenKey(reg.HKEY_USERS, path, 0, reg.KEY_WRITE)
+    reg.SetValueEx(key, value_name, 0, reg.REG_DWORD, value_data)
+    reg.CloseKey(key)
+
+def main():
+    run_regedit()
+    
+    username = None
+    while True:
+        username = input('Digite o nome do usuário: ').split(',')
+
+        if username:
+
+            sids = get_user_sid(username)
+
+            for sid in sids:
+                create_registry_key(f"{sid}\\Software\\Microsoft\Windows\\CurrentVersion\\Policies", "ActiveDesktop")
+
+                create_registry_value(f"{sid}\\Software\\Microsoft\Windows\\CurrentVersion\\Policies\\ActiveDesktop", "NoChangingWallPaper", 1)
+
+            print("NoChangingWallPaper registry created (value=true)! \nPower by: Nicolas & Pedro Lucas")
+
+            break
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
